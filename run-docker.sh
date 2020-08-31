3271d30a6e3682e7b10c909054401d7b8e441270
Ashwin Varghese Kuruttukulam <ashwinvk94@gmail.com>
2020-08-31 18:39:08 +0000

    add docker launch

--- run-docker.sh
+++ run-docker.sh
@@ -3,7 +3,7 @@
 xhost +local:root
 IMG=ashwinvk94/base:rpi
 
-docker run -it --user=$(id -u $USER):$(id -g $USER) --env="DISPLAY"\
+docker run -dit --restart=unless-stopped --user=$(id -u $USER):$(id -g $USER) --env="DISPLAY"\
         --workdir="/home/$USER" --volume="/home/$USER:/hone/$USER" \
        --volume="/etc/group:/etc/group:ro" --volume="/etc/passwd:/etc/passwd:ro" \
        --volume="/etc/shadow:/etc/shadow:ro" \
